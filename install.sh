d212ee14e5c1ab687672eecf37df3dfb8d92cbef
joao41 <joao41-second@users.noreply.github.com>
2025-11-04 17:41:15 +0000

    add cors_compile

---
+++ install.sh
@@ -0,0 +1,8 @@
+# Instalar pigpio
+sudo apt update
+sudo apt install cmake
+sudo apt install build-essential
+
+sudo apt install gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
+sudo apt install gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
+
